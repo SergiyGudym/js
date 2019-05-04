1d48b478fb0753008bdb99fb58f18761508a7d53
SergiyGudym <sergiygudym@gmail.com>
2019-05-04 17:51:39 +0300

    Add files via upload

--- hometask1.js
+++ hometask1.js
@@ -1,14 +1,20 @@
-let arr = [];
-
-let output = function(from, to) {
-  for (i=from; i<=to; i++) {
-    arr.push(i);
+let hometask1 = function() {
+  
+  let arr = [];
+  
+  let output = function(from, to) {
+    for (i=from; i<=to; i++) {
+      arr.push(i);
+    }
+    console.log(arr);
   }
-  console.log(arr);
-}
-
-let first = prompt("Please, set a first num", "0");
-let second = prompt("Please, set a second num", "0");
-let num1 = parseInt(first);
-let num2 = parseInt(second);
-output(num1, num2);
+  
+  let first = prompt("Please, set a first num", "0");
+  let second = prompt("Please, set a second num", "0");
+  let num1 = parseInt(first);
+  let num2 = parseInt(second);
+  output(num1, num2);
+  
+  }
+  
+hometask1();
